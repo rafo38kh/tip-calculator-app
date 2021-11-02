c8b6390de7e3e3e22ded3a539b292eaa7c3cd627
Rafael <khachikyan38@gmail.com>
2021-11-02 13:15:39 +0300

    buttons

--- js/script.js
+++ js/script.js
@@ -15,6 +15,18 @@ const LETTER_REGEX = /[a-zA-Z,/<>\?;':""[\]\\{}\|`~!@#\$%\^&\*()_=\+]+/g;
 
 const checkRegex = (reg, input) => new RegExp(reg).test(input);
 
+const customError = (elem) => {
+  elem.style.outline = "2px solid hsl(14, 32%, 57%)";
+  elem.style.color = "hsl(14, 32%, 57%)";
+  elem.style.caretColor = "hsl(14, 32%, 57%)";
+};
+
+const customSuccess = (elem) => {
+  elem.style.outline = "2px solid hsl(172, 67%, 45%)";
+  elem.style.color = "hsl(183, 100%, 15%)";
+  elem.style.caretColor = "hsl(172, 67%, 45%)";
+};
+
 const errorState = (elem, message, errorText) => {
   elem.style.outline = "2px solid hsl(14, 32%, 57%)";
   elem.style.color = "hsl(14, 32%, 57%)";
@@ -32,16 +44,13 @@ const successState = (elem, errorText) => {
 inputDollar.addEventListener("keyup", (e) => {
   const val = e.target.value;
 
-  if (!inputDollar.value || inputDollar.value === "0") {
+  if (!val || val === "0") {
     errorState(inputDollar, "Can't be zero", errorBill);
-  } else if (
-    checkRegex(LETTER_REGEX, inputDollar.value) ||
-    checkRegex(NUM_REGEX, inputDollar.value)
-  ) {
+  } else if (checkRegex(LETTER_REGEX, val) || checkRegex(NUM_REGEX, val)) {
     errorState(inputDollar, "Positive numbers only", errorBill);
-  } else if (checkRegex(ZERO_REGEX, inputDollar.value)) {
+  } else if (checkRegex(ZERO_REGEX, val)) {
     errorState(inputDollar, "Can't start with zero", errorBill);
-  } else if (inputDollar.value.split(".").length > 2) {
+  } else if (val.split(".").length > 2) {
     errorState(inputDollar, "can't have two dots", errorBill);
   } else {
     successState(inputDollar, errorBill);
@@ -51,16 +60,33 @@ inputDollar.addEventListener("keyup", (e) => {
 inputHuman.addEventListener("keyup", (e) => {
   const val = e.target.value;
 
-  if (!inputHuman.value || inputHuman.value === "0") {
+  if (!val || val === "0") {
     errorState(inputHuman, "Can't be zero", errorPeople);
-  } else if (
-    checkRegex(LETTER_REGEX, inputHuman.value) ||
-    checkRegex(PEOPLE_REGEX, inputHuman.value)
-  ) {
+  } else if (checkRegex(LETTER_REGEX, val) || checkRegex(PEOPLE_REGEX, val)) {
     errorState(inputHuman, "Positive numbers only", errorPeople);
-  } else if (checkRegex(ZERO_REGEX, inputHuman.value)) {
+  } else if (checkRegex(ZERO_REGEX, val)) {
     errorState(inputHuman, "Can't start with zero", errorPeople);
   } else {
     successState(inputHuman, errorPeople);
   }
+});
+
+inputTip.addEventListener("keyup", (e) => {
+  const val = e.target.value;
+  if (!val || val.split(".").length > 2) {
+    customError(inputTip);
+  } else if (checkRegex(NUM_REGEX, val)) {
+    customError(inputTip);
+  } else {
+    customSuccess(inputTip);
+  }
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) =>
+      btn.classList.remove("content__input-button-active")
+    );
+    e.target.classList.add("content__input-button-active");
+  });
 });
